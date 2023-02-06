436ae03daacdd7681eae7aa282377f024d01b62e
SIVASANKAR-JAYAVEL <sivasankar6104@gmail.com>
2023-02-06 22:33:29 +0530

    nodeJS.changes

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ app.use(express.json())
 
 //current date and time
 const dt = Date.now();
+console.log(dt);
+
 const date_obj = new Date(dt);
+console.log(date_obj);
+
 const date = date_obj.getDate();
+console.log(date);
+
 const month = date_obj.getMonth() + 1;
+console.log(month);
+
 const year = date_obj.getFullYear();
+console.log(year);
+
 const time = date_obj.getTime();
+console.log(time);
+
 const fileName = year + "-" + month + "-" + date + "-" + time;
+console.log(fileName);
 
 //content
 const timestamp = new Date().toISOString();
+console.log(timestamp);
 
 //file
 const fs = require("fs");
